898bd805d6c2a1c732bc6c8114974c669af178f5
ajefi <ajefimo2001@gmail.com>
2021-10-29 23:48:30 +0300

    save

--- src/Tnode.java
+++ src/Tnode.java
@@ -1,53 +1,63 @@
 import java.util.*;
 
 public class Tnode {
-   String name;
+   private String name;
    private Tnode firstChild; // left
    private Tnode nextSibling; // right
-   StringBuffer buffer = new StringBuffer();
+   //private List<Tnode> children = new ArrayList<>();
+   private Tnode parent;
+   static StringBuffer buffer = new StringBuffer();
 
-   public Tnode(String name) {
-      this.name = name;
+   public Tnode(Tnode parent) {
+      this.parent = parent;
    }
+   public String getName() {
+      return name;
+   }
+   public void setName(String name) {
+      this.name=name;
+   }
+   //public List<Tnode> getChildren() {
+   //   return children;
+   //}
+   //public Tnode getParent(){
+   //   return parent;
+   //}
 
-   public Tnode(String name, Tnode firstChild, Tnode nextSibling) {
-      this.name = name;
-      this.firstChild = firstChild;
-      this.nextSibling = nextSibling;
-   }
+   public Tnode() {}
+
+   //public Tnode(String name, Tnode firstChild, Tnode nextSibling) {
+   //   this.name = name;
+   //   this.firstChild = firstChild;
+   //   this.nextSibling = nextSibling;
+   //}
 
    @Override
    public String toString() {
       StringBuffer b = new StringBuffer();
       // TODO!!!
-      b.append(this.name);
+      // +[*{-(2,1),4},/(6,3)]
+
       Tnode node = this;
-      int i = 0;
-      while (node.firstChild != null) {
-         StringBuffer str = toStrHelper(node);
-         b.append(str);
-         node = this.firstChild;
-         System.out.println("node: " + node);
-         i++;
-      }
+      b.append(node.name);
+      b.append(node.getFirstChild());
+
       return b.toString();
    }
 
-   public StringBuffer toStrHelper(Tnode node) {
-      if (node.firstChild != null && node.nextSibling != null) {
-         buffer.append("(");
-         buffer.append(node.firstChild);
-         buffer.append(",");
-         buffer.append(node.nextSibling);
-         buffer.append(")");
-      }
-      if (node.firstChild == null) {
-         System.out.println(buffer.toString());
-         return buffer;
-      }
-      toStrHelper(node.firstChild);
-      toStrHelper(node.nextSibling);
+   public static StringBuffer toStrHelp(Tnode node) {
+      System.out.println("Node: " + node.getName());
+      buffer.append("(");
+      buffer.append(node.firstChild.getName());
+      toStrHelp(node.firstChild);
       return buffer;
+   }
+
+   private static Tnode addChild(Tnode parent, Tnode firstChild, Tnode nextSibling){
+      Tnode node = new Tnode(parent);
+      node.setFirstChild(firstChild);
+      node.setNextSibling(nextSibling);
+      return node;
    }
 
    public static Tnode buildFromRPN(String pol) {
@@ -56,8 +66,6 @@ public class Tnode {
       Tnode root = null;
       // TODO!!!
       String operators = "+-*/";
-
-
       String[] workWith = pol.split(" ");
       ArrayList<String> polArr = new ArrayList<>();
       Collections.addAll(polArr, workWith);
@@ -67,17 +75,24 @@ public class Tnode {
          if (operators.contains(polArr.get(n + 2))
                  && !operators.contains(polArr.get(n))
                  && !operators.contains(polArr.get(n + 1))) {
-            Tnode leftChild = new Tnode(polArr.get(n));
-            Tnode rightChild = new Tnode(polArr.get(n + 1));
-            Tnode node = new Tnode(polArr.get(n + 2), leftChild, rightChild);
+            // Tnode leftChild = new Tnode(polArr.get(n));
+            // Tnode rightChild = new Tnode(polArr.get(n + 1));
+            Tnode node = new Tnode();
+            node.setName(polArr.get(n+2));
+            Tnode firstChild = new Tnode();
+            firstChild.setName(polArr.get(n));
+            Tnode nextSibling = new Tnode();
+            nextSibling.setName(polArr.get(n + 1));
+            node = addChild(node, firstChild, nextSibling);
+
             polArr.remove(n + 1);
             polArr.remove(n);
             n = 0;
             root = node;
 
-            System.out.println("name: " + node.name);
-            System.out.println("left: " + leftChild);
-            System.out.println("right: " + rightChild);
+            System.out.println("name: " + node.getName());
+            System.out.println("left: " + node.firstChild.name);
+            System.out.println("right: " + node.getNextSibling().name);
             System.out.println();
 
             continue;
@@ -86,18 +101,25 @@ public class Tnode {
          if (operators.contains(polArr.get(n + 2))
                  && operators.contains(polArr.get(n))
                  && !operators.contains(polArr.get(n + 1))) {
-            Tnode leftChild = new Tnode(polArr.get(n));
-            Tnode rightChild = new Tnode(polArr.get(n + 1));
-            Tnode node = new Tnode(polArr.get(n + 2), leftChild, rightChild);
+            //Tnode leftChild = new Tnode(polArr.get(n));
+            //Tnode rightChild = new Tnode(polArr.get(n + 1));
+            Tnode node = new Tnode();
+            node.setName(polArr.get(n+2));
+            Tnode firstChild = new Tnode();
+            firstChild.setName(polArr.get(n));
+            Tnode nextSibling = new Tnode();
+            nextSibling.setName(polArr.get(n + 1));
+            addChild(node, firstChild, nextSibling);
+
             polArr.remove(n + 1);
             polArr.remove(n);
             n = 0;
             root = node;
 
-            System.out.println("name: " + node.name);
-            System.out.println("left: " + leftChild);
-            System.out.println("right: " + rightChild);
-            System.out.println();
+            //System.out.println("name: " + node.name);
+            //System.out.println("left: " + node.firstChild.name);
+            //System.out.println("right: " + node.getNextSibling().name);
+            //System.out.println();
 
             continue;
          }
@@ -105,18 +127,25 @@ public class Tnode {
          if (operators.contains(polArr.get(n + 2))
                  && operators.contains(polArr.get(n))
                  && operators.contains(polArr.get(n + 1))) {
-            Tnode leftChild = new Tnode(polArr.get(n));
-            Tnode rightChild = new Tnode(polArr.get(n + 1));
-            Tnode node = new Tnode(polArr.get(n + 2), leftChild, rightChild);
+            // Tnode leftChild = new Tnode(polArr.get(n));
+            // Tnode rightChild = new Tnode(polArr.get(n + 1));
+            Tnode node = new Tnode();
+            node.setName(polArr.get(n+2));
+            Tnode firstChild = new Tnode();
+            firstChild.setName(polArr.get(n));
+            Tnode nextSibling = new Tnode();
+            nextSibling.setName(polArr.get(n + 1));
+            addChild(node, firstChild, nextSibling);
+
             polArr.remove(n + 1);
             polArr.remove(n);
             n = 0;
             root = node;
 
-            System.out.println("name: " + node.name);
-            System.out.println("left: " + leftChild);
-            System.out.println("right: " + rightChild);
-            System.out.println();
+            //System.out.println("name: " + node.name);
+            //System.out.println("left: " + node.firstChild.name);
+            //System.out.println("right: " + node.getNextSibling().name);
+            //System.out.println();
 
             continue;
          }
@@ -129,8 +158,25 @@ public class Tnode {
       String rpn = "2 1 - 4 * 6 3 / +";
       System.out.println("RPN: " + rpn);
       Tnode res = buildFromRPN(rpn);
+      System.out.println("res: " + res);
       System.out.println("Tree: " + res);
       // TODO!!! Your tests here
+   }
+
+   public Tnode getFirstChild() {
+      return firstChild;
+   }
+
+   public void setFirstChild(Tnode firstChild) {
+      this.firstChild = firstChild;
+   }
+
+   public Tnode getNextSibling() {
+      return nextSibling;
+   }
+
+   public void setNextSibling(Tnode nextSibling) {
+      this.nextSibling = nextSibling;
    }
 }
 
